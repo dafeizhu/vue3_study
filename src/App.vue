f321328a081498cf0172378e65ebae2c34d1ef2e
xiaoniuaifengdou <xiaoniuaifengdou@gitee.com>
2023-06-28 11:09:15 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: huangzc
+ * @Date: 2023-06-28 09:37:48
+ * @LastEditors: huangzc
+ * @LastEditTime: 2023-06-28 09:52:25
+ * @Description: 
+-->
+<template>
+  <div>
+    <h1>APP根组件</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+/*
+ * @Author: huangzc
+ * @Date: 2023-06-28 09:37:48
+ * @LastEditors: huangzc
+ * @LastEditTime: 2023-06-28 09:41:30
+ * @Description: 
+ */
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
