26c7a85487850f7f921e92e485032820d8e8635c
xiaoniuaifengdou <xiaoniuaifengdou@gitee.com>
2023-06-28 11:17:56 +0800

    修改Readme

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@
  * @Author: huangzc
  * @Date: 2023-06-28 09:37:48
  * @LastEditors: huangzc
- * @LastEditTime: 2023-06-28 09:41:30
- * @Description: 
+ * @LastEditTime: 2023-06-28 11:15:22
+ * @Description:
  */
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
+
+const a = 100;
